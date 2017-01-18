a45d8db14069a6d54138a06084c5f72d3c0d080c
JMrbl <jmirabal15k@gmail.com>
2017-01-18 11:24:48 -0400

    Add category view adjustments in tablet mode

--- app/src/main/java/com/jsmirabal/appstoreexample/activity/MainActivity.java
+++ app/src/main/java/com/jsmirabal/appstoreexample/activity/MainActivity.java
@@ -5,26 +5,30 @@
 package com.jsmirabal.appstoreexample.activity;
 
 import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 import com.jsmirabal.appstoreexample.R;
 import com.jsmirabal.appstoreexample.utility.Util;
 
-import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EActivity;
 
 @EActivity(R.layout.activity_main)
 public class MainActivity extends AppCompatActivity {
     private static final String LOG_TAG = MainActivity.class.getSimpleName();
 
-    @AfterViews
-    void init() {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         changeTabletOrientation();
     }
 
     private void changeTabletOrientation() {
         if (Util.isTablet(this)) {
             setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        } else {
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         }
     }
 }
